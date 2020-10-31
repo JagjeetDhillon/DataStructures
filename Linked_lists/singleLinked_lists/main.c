02efd83894a42dbe34e4b56550c38365924799be
gautamdewasi <47949558+gautamdewasi@users.noreply.github.com>
2020-10-31 07:14:55 +0530

    become easy to read , adding documenation

--- Linked_lists/singleLinked_lists/main.c
+++ Linked_lists/singleLinked_lists/main.c
@@ -1,13 +1,17 @@
+// this program is used to create singly link list (adding nodes dynamically)and execute its all operations like adding nodes,deleting.displaying etc...
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
+// user define header
 #include"myHeader.h"
 
 int len;
 
+// creating a pointer root of node data type
 struct node* root = NULL;
 void main(){
 	int ch;
+	// to display all operations on link list
 	while(1){
 		//printf("Enter your choice\n");
 		printf("1.Append \n");
@@ -22,6 +26,8 @@ void main(){
 		printf("Enter your choice here: ");
 		scanf("%d", &ch);
 
+		// to execute any one case at a time
+		// these all functions (append(),insertAfront(),etc) are define in user define header called "myHeader.h"
 		switch(ch)
 		{
 			case 1: 
